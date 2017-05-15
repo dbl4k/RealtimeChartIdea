14897ad2c4df6b8175f01e2fee7b7e23104da68a
dbl4k <github@dbl4k.net>
2017-05-15 20:52:43 +0100

    remove data preformat from chart.js

--- RealtimeChartIdea/wwwroot/js/chart.js
+++ RealtimeChartIdea/wwwroot/js/chart.js
@@ -14,46 +14,13 @@ $(function () {
         responsive: true
     }; 
     
-    //var myChart = new Chart(ctx, {
-    //    type: 'line',
-    //    data: data,
-    //    options: options
-    //})
-
     setInterval(function () {
         $.ajax({
             url: "/api/chartdata/1"
-        }).done(function (dataSet) {
-
-            // initialize
-            if (this.data == null) {
-                this.data = { datasets: [] }
-            }
-
-            // Static bar graph operation.
-            //data.labels = dataSet.labels;
-            //data.datasets[0].data = dataSet.values;
-
-            // progressive line graph.
-            var length = dataSet.values.length;
-
-            for (var i = 0; i < length; i++) {
-
-                if (this.data.datasets.length < i) {
-                    this.data.datasets.push({ data: [dataSet.values[i]] });
-                } else {
-                    this.data.datasets[i].data.push(dataSet.values[i]);
-                }
-
-                
-                
-                //data.datasets[i].data.push(dataSet.values[i]);
-                //data.datasets[i].data.shift();
-            }
-            
+        }).done(function (dataSet) {           
             new Chart(ctx, {
                 type: 'line',
-                data: this.data,
+                data: dataSet,
                 options: options
             });
         });
